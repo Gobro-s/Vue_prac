74118242511c54c35723a330f04cbcf555efc540
chk7082 <whgusrl7082@gmail.com>
2023-05-02 17:48:37 +0900

    v-for사용

---
+++ 0502/vue/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="border">
+    <h1>This is My Component</h1>
+    <MyChild />
+    <MyChild />
+    <MyChild />
+  </div>
+</template>
+
+<script>
+import MyChild from '@/components/MyChild.vue'
+
+export default {
+  name: 'MyComponent',
+  components: {
+    MyChild,
+  }
+}
+</script>
+
+<style>
+.border {
+  border: solid;
+}
+</style>
